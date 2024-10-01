0007f54d61cc44e0cf670f229ea86202bdbcd86d
Rachel-Chen888 <rachel.luo887@gmail.com>
2024-10-01 16:27:12 -0400

    Added getter methods and fixed format and naming of code

--- app/src/userClasses/User.java
+++ app/src/userClasses/User.java
@@ -4,8 +4,27 @@ Used by the children classes to extend and get the variables inside that they al
  */
 
 public abstract class User { 
-    protected String firstName, lastName, email, password, addresses; 
-    protected String phoneNumber; 
+    protected String firstName, lastName, email, password, address, phoneNumber;  
 
-   
+    protected String getFirstName() {
+        return firstName; 
+    }
+
+    protected String getLastName() {
+        return getLastName; 
+    }
+
+    protected String getEmail() {
+        return email; 
+    }
+    
+    protected String getPassword() {
+        return password; 
+    }
+    protected String getAddress() {
+        return address; 
+    }
+    protected String getPhoneNumber() {
+        return phoneNumber; 
+    }
 }
