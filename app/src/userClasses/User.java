98ddd8b052fab15a2544907fcea315fd6945beb9
Rachel-Chen888 <rachel.luo887@gmail.com>
2024-10-01 16:25:02 -0400

    Variables to User abstract class and java docs added

--- app/src/userClasses/User.java
+++ app/src/userClasses/User.java
@@ -1,4 +1,11 @@
+/* 
+An Abstract class for user 
+Used by the children classes to extend and get the variables inside that they all share 
+ */
+
 public abstract class User { 
     protected String firstName, lastName, email, password, addresses; 
     protected String phoneNumber; 
+
+   
 }
