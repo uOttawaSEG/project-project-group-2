461e598b7493ae85e23a8a57b78d4ff8881f31a1
“Veronica <veronicanartatezbusiness@gmail.com>
2024-10-05 18:44:16 -0400

    fix: took out gradle lines in build.gradle.kts(module)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.constraintlayout.compose)
 }
