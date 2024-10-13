82a8dcfb439abd4626ba09a9c23c0f6ebd7d85ac
Isaac Jensen-Large <jensenlarge.isaac@gmail.com>
2024-10-13 00:09:04 -0400

    Made LoginSessionRepository more readable

--- app/src/main/java/ca/seg2105project/model/LoginSessionRepository.java
+++ app/src/main/java/ca/seg2105project/model/LoginSessionRepository.java
@@ -5,16 +5,29 @@ import android.content.SharedPreferences;
 
 public class LoginSessionRepository {
 
-    public static boolean isLoggedIn(String username, Context applicationContext){
-        SharedPreferences sharedPref = applicationContext.getSharedPreferences("session", Context.MODE_PRIVATE);
-        if(sharedPref.getString(username, null) == null){
-            return false;
-        }
-        return true;
+    /**
+     * Checks for existing shared preference extra "email" which contains the email of a user
+     * that has already successfully logged in
+     * @param applicationContext Android application context
+     * @return true if a user has logged in in a previous app session and has not yet logged out,
+     *  false otherwise
+     */
+    public static boolean hasActiveLoginSession(Context applicationContext){
+        SharedPreferences sharedPref = applicationContext.getSharedPreferences("session",
+                Context.MODE_PRIVATE);
+        return sharedPref.contains("email");
     }
 
-    public static void login(String email, Context applicationContext){
-        SharedPreferences sharedPref = applicationContext.getSharedPreferences("session", Context.MODE_PRIVATE);
+    /**
+     * Method to store successfully authenticated (see UserRepository.authenticateEmailAndPassowrd)
+     * user email. This is used to maintain user login session across all app instances until the
+     * user specifically logs out
+     * @param email the email of the successfully authenticated user
+     * @param applicationContext Android application context
+     */
+    public static void startLoginSession(String email, Context applicationContext){
+        SharedPreferences sharedPref = applicationContext.getSharedPreferences("session",
+                Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString("email", email);
         editor.apply();
